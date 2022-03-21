d8e0f982387717071b203470d1147a4e6cdb1a30
youremail@domain.com <alenakilikli@gmail.com>
2022-03-21 22:12:52 +0100

    added class Main , replaced mainMethod

--- src/firstProject/epic/CSVReaderDemo.java
+++ src/firstProject/epic/CSVReaderDemo.java
@@ -9,16 +9,6 @@ public class CSVReaderDemo {
 
     String csvFile;
 
-    public static void main(String[] args) {
-        String csvFile = "UrlList.csv";
-        if (args.length > 0) {
-            csvFile = args[0];
-        }
-        List<PictureFiles> picUrls = readFromCsvFile(csvFile);
-        for (PictureFiles p : picUrls) {
-            System.out.println(p );
-        }
-    }
 
     private static PictureFiles createFilesList(String[] metadata) {
 
